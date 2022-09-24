78e94473d76af8715e95c8f5ee56ae54bb3dc897
dipro <toriqul.islam@egeneration.co>
2022-09-24 12:58:18 +0600

    first commit

---
+++ src/app/crud-comp/crud-comp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-crud-comp',
+  templateUrl: './crud-comp.component.html',
+  styleUrls: ['./crud-comp.component.scss']
+})
+export class CrudCompComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
